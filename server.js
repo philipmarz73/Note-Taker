e65fb939ed0407a06f39fa8077e8b8f3a6cee0ae
Phlipper <marzullop73@gmail.com>
2020-11-27 15:25:55 -0500

    added routes and server

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var htmlRoutes = require("./routes/htmlRoutes");
+
+
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"))
+app.use("/", htmlRoutes)
+
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
