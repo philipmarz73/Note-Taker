e5029597b5b6e625b13a86d803abe68e948e26e9
Phlipper <marzullop73@gmail.com>
2021-02-13 21:21:29 -0500

    add comments to note.js file

--- db/note.js
+++ db/note.js
@@ -1,14 +1,20 @@
+//create note.js file to add notes, retrieve notes, or remove notes from JSON
+
+// adding required files from npm packages
+
 const { notEqual } = require("assert");
 const fs = require ("fs");
 
 const util = require ("util");
 
 const {v4:uuidv4} = require ("uuid");
+// file for auto-generated id
 
 const readFileAsynch = util.promisify(fs.readFile);
 const writeFileAsynch = util.promisify(fs.writeFile);
 
-
+// creates basic functionality for your input notes,
+// read, write, return, add, remove
 class Note {
 
     read(){
@@ -18,7 +24,7 @@ class Note {
     write(note){
         return writeFileAsynch("db/db.json",JSON.stringify(note))
     }
-
+// check notes for error, if error-free, return output
     getNotes(){
         return this.read().then(notes => {
             let parseNotes;
@@ -31,6 +37,7 @@ class Note {
             return parseNotes;
         })
     }
+    // update notes if new id generated, showing new notes
     addNote(note){
         const {title, text} = note;
         const newNote = {title, text, id:uuidv4()};
